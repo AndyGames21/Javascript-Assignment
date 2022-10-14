3a532e24ef5ae4b9cc384a71e8ac750551413041
AndyGames21 <77165906+AndyGames21@users.noreply.github.com>
2022-10-14 20:22:36 +0100

    Assignment Done

--- Assignment.js
+++ Assignment.js
@@ -1,4 +1,4 @@
-const kelvin="293"
+const kelvin="0"
 //Const is what will be used for this
 const celsius = kelvin - 273
 //What happened here is that we made a variable called celsius and to get its value we did kelvin=293 - 273=20
