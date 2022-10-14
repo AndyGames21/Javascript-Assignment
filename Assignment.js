03c5d2795fba00e1dbfdc74f1eb4b6bcf4d2b8cf
AndyGames21 <77165906+AndyGames21@users.noreply.github.com>
2022-10-14 20:20:04 +0100

    Work Almost Done

--- Assignment.js
+++ Assignment.js
@@ -2,3 +2,9 @@ const kelvin="293"
 //Const is what will be used for this
 const celsius = kelvin - 273
 //What happened here is that we made a variable called celsius and to get its value we did kelvin=293 - 273=20
+fahrenheit = celsius * (9/5) + 32
+let fahrenheit = celsius * (9 / 5) + 32;
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+
+
+ 
